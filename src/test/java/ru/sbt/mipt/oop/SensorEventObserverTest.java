f6153889c4f22632ddd04b954d12d8907df4bee3
EnjeruGorkaya <tanya.meow13@gmail.com>
2017-12-16 14:37:05 +0300

    Добавлен тест на observer

--- src/test/java/ru/sbt/mipt/oop/SensorEventObserverTest.java
+++ src/test/java/ru/sbt/mipt/oop/SensorEventObserverTest.java
@@ -1,6 +1,15 @@
 package ru.sbt.mipt.oop;
 
 import org.junit.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
@@ -8,21 +17,22 @@ import static org.junit.Assert.*;
  * Created by Enjeru on 09.12.2017.
  */
 public class SensorEventObserverTest {
-    @Test
-    // создаем новое событие, ожидаем "Got event: имя ивента"
-    public void TestGettingEventName () {
-
-    }
 
     @Test
     // проверяем добавление хэндлера
     public  void  TestAddHandler () {
+        // Подготовка
+        SmartHome smartHome = mock(SmartHome.class);
+        LightEventProcessing lightEventHandler = mock(LightEventProcessing.class);
+        DoorEventProcessing doorEventHandler = mock(DoorEventProcessing.class);
+        SensorEvent sensorEvent = mock(SensorEvent.class);
+        SensorEventObserver observer = new SensorEventObserver(smartHome);
 
+        // Тест
+        observer.addHandler(lightEventHandler);
+        observer.addHandler(doorEventHandler);
+        verify(doorEventHandler).handle(smartHome, sensorEvent);
+        verify(lightEventHandler).handle(smartHome, sensorEvent);
     }
 
-    @Test
-    // проверяем установку хэндлера
-    public void TestSetHandlers () {
-
-    }
 }
